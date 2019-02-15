e83f08037f252a4ac611836a572a11ca30d48258
Piotr Biniek <piotr.biniek@accenture.com>
2019-02-15 17:05:56 +0100

    wstępne testy, rozwiązac problem zuul i autoryzacji tokenem poprzez zuul

--- MyFamily_calendar_microservice/src/main/java/pl/biniek/FamilyCalendarMicroserviceApplication.java
+++ MyFamily_calendar_microservice/src/main/java/pl/biniek/FamilyCalendarMicroserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableDiscoveryClient
+@EnableResourceServer
 public class FamilyCalendarMicroserviceApplication {
 
 	public static void main(String[] args) {
